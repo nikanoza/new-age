021d115290bca7b63755ddbc6df19270de7a89e3
hilu <nikanozadze1010@gmail.com>
2022-11-26 16:21:58 +0400

    feat: add vue-router

--- new-age/src/App.vue
+++ new-age/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-   
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {}
-}
+  name: "App",
+  components: {},
+};
 </script>
 
-<style lang="scss"  scoped>
-  @import "app.scss"
+<style lang="scss" scoped>
+@import "app.scss";
 </style>
--- new-age/src/main.js
+++ new-age/src/main.js
@@ -1,9 +1,20 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
 import "./main.scss";
+import SignUp from "./views/SignUp.vue";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    { path: "/", redirect: "/sign-up" },
+    { path: "/sign-up", component: SignUp },
+  ],
+});
 
 new Vue({
   render: (h) => h(App),
+  router,
 }).$mount("#app");
