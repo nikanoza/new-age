94592dc4683325810215dd13d33db4025de15d8c
hilu <nikanozadze1010@gmail.com>
2022-11-26 13:00:21 +0400

    feat: create and setup vue project

---
+++ new-age/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+   
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style lang="scss"  scoped>
+  @import "app.scss"
+</style>
