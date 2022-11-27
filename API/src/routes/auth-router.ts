10173402285bd666dd4ea9cb64a226362725d558
hilu <nikanozadze1010@gmail.com>
2022-11-27 09:34:25 +0400

    feat: add sign in post endpoint and also create swagger path for this route

--- API/src/routes/auth-router.ts
+++ API/src/routes/auth-router.ts
@@ -1,8 +1,9 @@
-import { signUp } from "controllers";
+import { SignIn, signUp } from "controllers";
 import express from "express";
 
 const authRouter = express.Router();
 
 authRouter.post("/auth/sign-up", signUp);
+authRouter.post("/auth/sign-in", SignIn);
 
 export default authRouter;
